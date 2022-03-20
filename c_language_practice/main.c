ee2ff6f197785f6e60553848b398eb06471a9533
kyungjunleeme <kyungjunlee.me@gmail.com>
2022-03-20 15:36:35 +0900

    Initial Commit

---
+++ c_language_practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_language_practice
+//
+//  Created by Kyungjun Lee on 2022/03/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
